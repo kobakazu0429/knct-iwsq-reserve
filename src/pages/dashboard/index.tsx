c7f04363714b52074f80c2f8a3c85bcb3f67b923
kobakazu0429 <kobakazu10@gmail.com>
2023-03-25 14:49:05 +0900

    feat: add some pages

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import type { NextPage } from "next";
+import { Dashboard } from "../../layouts/dashboard";
+
+const DashboardPage: NextPage = () => {
+  return (
+    <Dashboard>
+      <span>DashboardPage / coming soon...</span>
+    </Dashboard>
+  );
+};
+
+export default DashboardPage;
---
+++ src/pages/dashboard/mails/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import type { NextPage } from "next";
+import { Dashboard } from "../../../layouts/dashboard";
+
+const DashboardMails: NextPage = () => {
+  return (
+    <Dashboard>
+      <span>DashboardMails / coming soon...</span>
+    </Dashboard>
+  );
+};
+
+export default DashboardMails;
